71f78b5ea606a1615dc04fcfad644eeda5eecd0c
Andrew Kolisz <andrew@1926marketing.com>
2017-07-13 13:23:50 -0400

    added correct styling for anchored-header

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,32 +7,3 @@ $(function () {
 		$(".menu").toggleClass("open");
 	});
 });
-
-// Studio Trailer
-
-var PLAY_VIDEO_COMMAND = JSON.stringify({func: "playVideo", event: "command", args: ""})
-var PAUSE_VIDEO_COMMAND = JSON.stringify({func: "pauseVideo", event: "command", args: ""})
-
-function stopStudioTrailer() {
-  if($("#studio-trailer").hasClass("in-pop-up")) {
-    $("#studio-trailer").removeClass("in-pop-up")
-    $("#studio-trailer").find("iframe").get(0).contentWindow.postMessage(PAUSE_VIDEO_COMMAND, '*')
-  }
-}
-
-function playStudioTrailer() {
-  $("#studio-trailer").addClass("in-pop-up")
-  $("#studio-trailer").find("iframe").get(0).contentWindow.postMessage(PLAY_VIDEO_COMMAND, '*')
-}
-
-$("[href='#play']").on("click", function(event) {
-  playStudioTrailer()
-})
-
-$("#studio-trailer").on("click", function(event) {
-  stopStudioTrailer()
-})
-
-$(document).on("scroll", function(event) {
-  stopStudioTrailer()
-})
