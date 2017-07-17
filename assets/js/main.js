46e91f19a4f84116e88298f3b5a0c923adeb384e
Andrew Kolisz <andrew@1926marketing.com>
2017-07-17 10:19:42 -0400

    update changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
-// Navigation Menu JS
-
-$(function () {
-	$(".menu-link").click(function (e) {
-		e.preventDefault();
-		$(".menu-overlay").toggleClass("open");
-		$(".menu").toggleClass("open");
+$(document).ready(function () {
+	$('.hamburger').click(function () {
+		$('.hamburger, .wrapper').toggleClass('is-active');
 	});
 });
+
+// Navigation Menu JS
+
+//$(function () {
+//	$(".menu-link").click(function (e) {
+//		e.preventDefault();
+//		$(".menu-overlay").toggleClass("open");
+//		$(".menu").toggleClass("open");
+//	});
+//});
